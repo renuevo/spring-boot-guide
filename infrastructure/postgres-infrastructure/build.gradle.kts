ecd5221a4a209ab1138f086bfd31b342a20ad9e8
renuevo <renuevo0110@gmail.com>
2021-03-30 21:05:21 +0900

    :wrench: Postgres Setting

--- infrastructure/postgres-infrastructure/build.gradle.kts
+++ infrastructure/postgres-infrastructure/build.gradle.kts
@@ -1,9 +1,9 @@
-
 val querydslVersion = "4.4.0"
 val annotationApiVersion = "1.3.2"
 
 
 dependencies {
+    runtimeOnly("com.h2database:h2")
     implementation("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
