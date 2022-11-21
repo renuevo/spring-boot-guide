bf35acbe07c5ffdc1565bb128ce9356243b655e6
renuevo <renuevo0110@gmail.com>
2022-11-22 00:14:24 +0900

    :wrench: change gradle include pattern

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -1,6 +1,6 @@
 object Projects {
     const val domain = ":domain"
     const val client = ":client"
-    const val appCommonApi = ":app-common-api"
-    const val postgresInfrastructure = ":postgres-infrastructure"
+    const val appCommonApi = ":app:common-api"
+    const val postgresInfrastructure = ":infrastructure:postgres-infrastructure"
 }
